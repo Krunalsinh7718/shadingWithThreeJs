7032c9e1e1701ec8e71b5ee8387e702c940cc684
AT-KV1425 <krunalsinh.vaghela@arianus.in>
2026-04-05 10:10:41 +0530

    changes

---
+++ src/pages/basic/main.js
@@ -0,0 +1,77 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import GUI from 'lil-gui'
+import testVertexShader from './vertex.vert?raw'
+import testFragmentShader from './fragment.frag?raw'
+
+
+
+//sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+//scene setup
+const scene = new THREE.Scene();
+
+//camera setup
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(1, 1, 1)
+scene.add(camera)
+
+//renderer setup
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(sizes.width, sizes.height)
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+//mesh setup
+// Geometry
+const geometry = new THREE.PlaneGeometry(2, 2, 512, 512);
+
+// Material
+const material = new THREE.ShaderMaterial({
+    vertexShader: testVertexShader,
+    fragmentShader: testFragmentShader
+});
+
+// Mesh
+const mesh = new THREE.Mesh(geometry, material);
+mesh.rotation.x = - Math.PI * 0.5;
+scene.add(mesh);
+
+//controls setup
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+
+const clock = new THREE.Clock();
+
+//animation loop
+function animate() {
+
+    const elapsedTime = clock.getElapsedTime();
+
+    //update controls
+    controls.update();
+
+    //render
+    renderer.render(scene, camera);
+}
+
+//handle window resize
+window.addEventListener('resize', () => {
+
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+});

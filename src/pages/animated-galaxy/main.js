e42e038382410ae0a7ae45893f526d0f236f4c24
AT-KV1425 <krunalsinh.vaghela@arianus.in>
2026-04-10 10:10:02 +0530

    changes

--- src/pages/animated-galaxy/main.js
+++ src/pages/animated-galaxy/main.js
@@ -6,16 +6,8 @@ import testFragmentShader from './shaders/fragment.frag'
 
 //gui
 const gui = new GUI({width: 340});
-const parameters = {}
-parameters.count = 200000
-parameters.size = 0.005
-parameters.radius = 5
-parameters.branches = 3
-parameters.spin = 1
-parameters.randomness = 0.5
-parameters.randomnessPower = 3
-parameters.insideColor = '#ff6030'
-parameters.outsideColor = '#1b3984'
+
+
 
 
 //sizes
@@ -39,92 +31,62 @@ renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
 //galaxy
+const parameters = {};
+parameters.count = 2000;
+parameters.pointSize = 0.01;
+parameters.radius = 5;
+parameters.branches = 3;
+parameters.randomness = 0.5
+parameters.randomnessPower = 3
 
-let geometry = null
-let material = null
-let points = null
+let pointGeometry = null;
+let pointMaterial = null;
+let points = null;
 
-const generateGalaxy = () =>
-{
-    if(points !== null)
-    {
-        geometry.dispose()
-        material.dispose()
-        scene.remove(points)
+const generateGalaxy = () => {
+    if(points !== null){
+        pointGeometry.dispose();
+        pointMaterial.dispose();
+        scene.remove(points);
     }
 
-    /**
-     * Geometry
-     */
-    geometry = new THREE.BufferGeometry()
+    const pointsPositionArr = new Float32Array(parameters.count * 3);
+    const pointsColorArr = new Float32Array(parameters.count * 3);
 
-    const positions = new Float32Array(parameters.count * 3)
-    const colors = new Float32Array(parameters.count * 3)
+    for (let i = 0; i < parameters.count; i++) {
+        const i3 = i * 3;
+        const radius = Math.random() * parameters.radius;
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2;
 
-    const insideColor = new THREE.Color(parameters.insideColor)
-    const outsideColor = new THREE.Color(parameters.outsideColor)
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() > 0.5 ? 1 : -1) * radius * parameters.randomness;
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() > 0.5 ? 1 : -1) * radius * parameters.randomness;
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() > 0.5 ? 1 : -1) * radius * parameters.randomness;
 
-    for(let i = 0; i < parameters.count; i++)
-    {
-        const i3 = i * 3
+        pointsPositionArr[i3    ] = Math.cos(branchAngle) * radius + randomX;
+        pointsPositionArr[i3 + 1] = randomY;
+        pointsPositionArr[i3 + 2] = Math.sin(branchAngle) * radius + randomZ;
 
-        // Position
-        const radius = Math.random() * parameters.radius
-
-        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
-        console.log((i % parameters.branches) / parameters.branches);
+        pointsColorArr[i3    ] = 0.1;
+        pointsColorArr[i3 + 1] = 1.0;
+        pointsColorArr[i3 + 2] = 0.5;
         
-
-        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-
-        positions[i3    ] = Math.cos(branchAngle) * radius + randomX
-        positions[i3 + 1] = randomY
-        positions[i3 + 2] = Math.sin(branchAngle) * radius + randomZ
-
-        // Color
-        const mixedColor = insideColor.clone()
-        mixedColor.lerp(outsideColor, radius / parameters.radius)
-
-        colors[i3    ] = mixedColor.r
-        colors[i3 + 1] = mixedColor.g
-        colors[i3 + 2] = mixedColor.b
     }
 
-    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-
-    /**
-     * Material
-     */
-    material = new THREE.PointsMaterial({
-        size: parameters.size,
+    pointGeometry = new THREE.BufferGeometry();
+    pointGeometry.setAttribute('position', new THREE.BufferAttribute(pointsPositionArr, 3));
+    pointGeometry.setAttribute('color', new THREE.BufferAttribute(pointsColorArr, 3));
+    pointMaterial = new THREE.PointsMaterial({
+        size : parameters.pointSize,
         sizeAttenuation: true,
         depthWrite: false,
         blending: THREE.AdditiveBlending,
         vertexColors: true
     })
 
-    /**
-     * Points
-     */
-    points = new THREE.Points(geometry, material)
-    scene.add(points)
+    points = new THREE.Points(pointGeometry, pointMaterial);
+    scene.add(points);
 }
-
-generateGalaxy()
-
-gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
-gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
-gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
-
-
-
+generateGalaxy();
 
 
 //controls setup
